0395f5a42889228bb9fc330882fefa71085a3c82
Max <38158634+maxdraz@users.noreply.github.com>
2019-03-12 19:55:52 +0000

    path following WIP

---
+++ GE2 Space Battle Unity/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : SteeringBehaviour
+{
+
+    public override Vector3 Calculate()
+    {
+        throw new System.NotImplementedException();
+    }
+}

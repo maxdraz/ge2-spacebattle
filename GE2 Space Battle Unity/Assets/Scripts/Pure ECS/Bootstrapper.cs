dbf2599be65047dd034689665834873a555631fb
Max <38158634+maxdraz@users.noreply.github.com>
2019-05-01 10:14:50 +0100

    upfated vis studio

---
+++ GE2 Space Battle Unity/Assets/Scripts/Pure ECS/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//add ECS namespace
+using Unity.Entities;
+
+
+public class Bootstrapper : MonoBehaviour
+{
+    public struct Move : IComponentData
+    {
+        public float moveSpeed;
+    }
+}
